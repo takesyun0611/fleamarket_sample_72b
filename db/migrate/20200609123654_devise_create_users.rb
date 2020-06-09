91f7a91ddeca66babcaab26cb9f2df27ae36acef
Kishou <xijie931025@gmail.com>
2020-06-09 21:48:07 +0900

    migrate編集

--- db/migrate/20200609123654_devise_create_users.rb
+++ db/migrate/20200609123654_devise_create_users.rb
@@ -4,6 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname, null:false
+      t.string :icon
+      t.string :family_name, null: false
+      t.string :given_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :given_name_kana, null: false
+      t.date :birthday, null: false
+      t.string :phone_number
+      t.text :intro
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 

981c03c071c7ec465d022c9b1ff611a3171da266
Kishou <xijie931025@gmail.com>
2020-06-13 14:37:13 +0900

    shipments_validate

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,9 @@
 class Shipment < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
+
+  [family_name, given_name, family_name_kana, given_name_kana, postal_code, city, house_number].each do |v|
+    validates v,
+      presence: true
+  end
 end

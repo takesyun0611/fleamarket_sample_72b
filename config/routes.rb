0c92320b45bcf0c805dca09c2d0240d364b05c2f
Kishou <xijie931025@gmail.com>
2020-06-13 16:42:09 +0900

    routes_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  devise_scope :user do
+    get 'shipments', to: 'users/registrations#new_shipment'
+    post 'shipments', to: 'users/registrations#create_shipment'
+  end
   root 'items#index'
 end

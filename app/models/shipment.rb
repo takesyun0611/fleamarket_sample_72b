3969f0982a8c96bf170ad5833f22af881c690177
Kishou <xijie931025@gmail.com>
2020-06-14 00:24:23 +0900

    model_shipment

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -6,6 +6,6 @@ class Shipment < ApplicationRecord
   validates :house_number, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :given_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :given_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end

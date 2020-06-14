03eef9cc918f1a9b338339910c8ae9ea81c624fd
Kishou <xijie931025@gmail.com>
2020-06-14 19:13:43 +0900

    shipment_save

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -33,6 +33,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.save
     session["devise.regist_data"]["user"].clear
     sign_in(:user, @user)
+    redirect_to root_path
   end
 
   # GET /resource/edit

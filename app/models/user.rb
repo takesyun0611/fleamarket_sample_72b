d909d1e5548d056f7b3af51b53e187a258ee5d78
Kishou <xijie931025@gmail.com>
2020-06-14 19:05:43 +0900

    user_validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   validates :birthday, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :given_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :given_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end

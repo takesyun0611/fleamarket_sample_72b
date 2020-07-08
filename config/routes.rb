1c974a2ea88d4384db57b8dffd2a67bdfb91799c
takesyun0611 <take.shun1616@gmail.com>
2020-07-09 00:10:02 +0900

    cardルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :products, only: [:index, :create]
   resources :users, only: :show do
-    resources :cards, only: [:new, :create, :edit]
+    resources :cards, only: [:new, :create, :edit, :show]
   end
   resources :products, except: :show do
     collection do

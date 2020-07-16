8bb4c24185278357dbd4899bd8563199a0e6bd7e
takesyun0611 <take.shun1616@gmail.com>
2020-07-16 22:58:03 +0900

    単体テスト

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    it "customer_idとcard_idが存在して、userと紐付いてるなら登録できること" do
+      user = create(:user)
+      card = build(:card, user: user)
+      expect(card).to be_valid
+    end
+
+    it "userと紐付いてないと登録できないこと" do
+      user = build(:user) # buildで作成するとuser_idがnilになる
+      card = build(:card, user: user)
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "customer_idがなければ登録できないこと" do
+      user = create(:user)
+      card = build(:card, customer_id: nil, user: user)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "card_idがなければ登録できないこと" do
+      user = create(:user)
+      card = build(:card, card_id: nil, user: user)
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+  end
 end

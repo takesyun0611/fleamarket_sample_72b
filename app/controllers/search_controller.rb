bd2fad482e244206d8e7156ebd3da2ff0b00680d
Kishou <xijie931025@gmail.com>
2020-07-26 21:06:37 +0900

    不要コメント削除

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # binding.pry
     @q = Product.ransack(params[:q])
     @category_parents = Category.all.order("id ASC").limit(13)
     @category = Category.find_by(id: params["q"]["category_id"])

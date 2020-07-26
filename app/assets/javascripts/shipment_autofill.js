7ecb170fa26041edb4cfe79c7ef3ae357efb65c8
taka <taka7930119@icloud.com>
2020-07-26 15:53:44 +0900

    ユーザー情報編集【配送先住所】(修正2
    ページに遷移した際にturbolinksの影響で１度リロードしないと、郵便番号を書き換えたときに都道府県・市区町村が自動で入力されない。

--- app/assets/javascripts/shipment_autofill.js
+++ app/assets/javascripts/shipment_autofill.js
@@ -1,9 +1,11 @@
-$(function() {
-  return $('#shipment_postal_code').jpostal({
-    postcode: ['#shipment_postal_code'],
-    address: {
-      '#shipment_prefecture': '%3',
-      '#shipment_city': '%4%5%6%7',
-    },
+$(document).on('DOMContentLoaded turbolinks:render', function() {
+  $(function() {
+    return $('#shipment_postal_code').jpostal({
+      postcode: ['#shipment_postal_code'],
+      address: {
+        '#shipment_prefecture': '%3',
+        '#shipment_city': '%4%5%6%7',
+      },
+    });
   });
 });

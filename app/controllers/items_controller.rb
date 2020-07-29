bec7af6a7ec06ed456ddd4da53d25308630b0fce
tetuworld <teppei.kawano11@gmail.com>
2020-07-29 20:45:48 +0900

    商品一覧表示追加実装カテゴリー別

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,8 @@ class ItemsController < ApplicationController
   def index
     @products = Product.includes(:pictures).order('created_at DESC').limit(10)
     @q = Product.ransack(params[:q])
+    @categorymen = Product.order("RAND()").where('category_id <= 353').where('category_id >= 211').limit(3)
+    @categorylady = Product.order("RAND()").where('category_id <= 210').where('category_id >= 14').limit(3)
+    @categorybooks = Product.order("RAND()").where('category_id <= 684').where('category_id >= 627').limit(3)
   end
 end

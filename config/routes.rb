274e3aa0ecde59e30616cbf50a6ad5f4ee5961be
muraoka1229 <muratomo1991@yahoo.co.jp>
2020-08-07 12:00:22 +0900

    コメントアウトの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users, controllers: {
-  #   registrations: 'users/registrations',
-  # }
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
